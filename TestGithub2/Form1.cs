14c08afa7008cb4d6ad3e3a753367201573e77db
SudeshK <sudeshk@datadevice.com.au>
2019-05-16 12:37:54 +0530

    Test02

--- TestGithub2/Form1.cs
+++ TestGithub2/Form1.cs
@@ -16,5 +16,10 @@ namespace TestGithub2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Hi
+        }
     }
 }
