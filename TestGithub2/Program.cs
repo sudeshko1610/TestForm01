44313cd83794f59a370ba2690e1d9f72493ad041
rajas <rajahs@datadevice.com.au>
2019-05-16 12:49:45 +0530

    added read

--- TestGithub2/Program.cs
+++ TestGithub2/Program.cs
@@ -17,6 +17,7 @@ namespace TestGithub2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.Read();
         }
     }
 }
